d647b3c7ec1d47803ac2b127831dd22af64a08da
joisgori <37356528+joisgori@users.noreply.github.com>
2018-05-01 12:17:00 -0600

    Actualizando usuario
    Esta clase hereda de la clase persona, y se agregaron sus atributos y métodos

--- src/VillaRafinha/Usuario.java
+++ src/VillaRafinha/Usuario.java
@@ -6,9 +6,39 @@
 package VillaRafinha;
 
 /**
+ * Usuario, Huesped, Persona, TarjetaCredito, Admin
  *
- * @author daniel
+ * @author Josué González <00034715@uca.edu.sv>
  */
-public class Usuario {
-    
+public class Usuario extends Persona {
+
+    String usuario;
+    String constraseña;
+//constructores
+
+    public Usuario() {
+    }
+
+    public Usuario(String usuario, String constraseña) {
+        this.usuario = usuario;
+        this.constraseña = constraseña;
+    }
+//métodos getter
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public String getConstraseña() {
+        return constraseña;
+    }
+//métodos setter
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public void setConstraseña(String constraseña) {
+        this.constraseña = constraseña;
+    }
 }
