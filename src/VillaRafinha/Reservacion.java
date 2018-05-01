4e07f9cda0e11542a92cdbbd6a89edb4e63ddacd
daniel <daniel@daniel-HP-1000-Notebook-PC>
2018-04-30 19:55:49 -0600

    Estructura basica de Reservacion y Hospedaje

--- src/VillaRafinha/Reservacion.java
+++ src/VillaRafinha/Reservacion.java
@@ -5,10 +5,107 @@
  */
 package VillaRafinha;
 
+import java.util.Objects;
+
 /**
  *
  * @author daniel
  */
+
+
 public class Reservacion {
+    
+    private boolean sencilla;
+    private Huesped huesped;
+    private Hospedaje estancia;
+    private Paquete paquete;
+    private Float total;
+    private boolean disponible;
+    private TarjetaCredito garantia;
+
+    public void setSencilla(boolean sencilla) {
+        this.sencilla = sencilla;
+    }
+
+    public void setHuesped(Huesped huesped) {
+        this.huesped = huesped;
+    }
+
+    public void setEstancia(Hospedaje estancia) {
+        this.estancia = estancia;
+    }
+
+    public void setPaquete(Paquete paquete) {
+        this.paquete = paquete;
+    }
+
+    public void setTotal(Float total) {
+        this.total = total;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    public boolean isSencilla() {
+        return sencilla;
+    }
+
+    public Huesped getHuesped() {
+        return huesped;
+    }
+
+    public Hospedaje getEstancia() {
+        return estancia;
+    }
+
+    public Paquete getPaquete() {
+        return paquete;
+    }
+
+    public Float getTotal() {
+        return total;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 47 * hash + Objects.hashCode(this.huesped);
+        hash = 47 * hash + Objects.hashCode(this.estancia);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Reservacion other = (Reservacion) obj;
+        if (!Objects.equals(this.huesped, other.huesped)) {
+            return false;
+        }
+        if (!Objects.equals(this.estancia, other.estancia)) {
+            return false;
+        }
+        return true;
+    }
+    
+    public void cancelarReservacion(){
+        this.setDisponible(false);
+    }
+    
+    public void procesarREservacion(TarjetaCredito t){
+        this.garantia=t;
+    }
     
 }
