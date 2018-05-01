b238dabdb67417a6e7ef89001dd5f4edcadf05a8
joisgori <37356528+joisgori@users.noreply.github.com>
2018-05-01 12:10:45 -0600

    Update Admin.java
    Agregué todos los métodos y constructores faltantes a la clase admin

--- src/VillaRafinha/Admin.java
+++ src/VillaRafinha/Admin.java
@@ -7,8 +7,39 @@ package VillaRafinha;
 
 /**
  *
- * @author daniel
+ * @author Josué González <00034715@uca.edu.sv>
  */
 public class Admin {
+    Usuario usuario;
+    int idAdmin;
+    
+    //constructores
+
+    public Admin() {
+    }
+
+    public Admin(Usuario usuario, int idAdmin) {
+        this.usuario = usuario;
+        this.idAdmin = idAdmin;
+    }
+    
+    //métodos getter
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public int getIdAdmin() {
+        return idAdmin;
+    }
+
+    //métodos setter
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public void setIdAdmin(int idAdmin) {
+        this.idAdmin = idAdmin;
+    }
     
 }
