1984466afc7d85a6e8c73ae9225251a194415167
joisgori <37356528+joisgori@users.noreply.github.com>
2018-05-01 12:13:48 -0600

    Actualizando Persona
    Agregué todos sus constructores y sus atributos junto a métodos getter y setters

--- src/VillaRafinha/Persona.java
+++ src/VillaRafinha/Persona.java
@@ -10,5 +10,45 @@ package VillaRafinha;
  * @author daniel
  */
 public class Persona {
-    
+
+    String nombre;
+    DUI dui;
+    int telefono;
+//constructores
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, DUI dui, int telefono) {
+        this.nombre = nombre;
+        this.dui = dui;
+        this.telefono = telefono;
+    }
+//métodos getter
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public DUI getDui() {
+        return dui;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+//métodos setter
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDui(DUI dui) {
+        this.dui = dui;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
 }
