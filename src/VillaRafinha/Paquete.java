ee78d98675c0ea395fa109e6fe7a0e6fd9b081e9
aacm13 <37373620+aacm13@users.noreply.github.com>
2018-05-01 21:33:13 -0600

    Update Paquete.java

--- src/VillaRafinha/Paquete.java
+++ src/VillaRafinha/Paquete.java
@@ -4,11 +4,45 @@
  * and open the template in the editor.
  */
 package VillaRafinha;
-
+import java.util.*;
 /**
  *
- * @author daniel
+ * @author aacm13
  */
 public class Paquete {
+    private tipoPaquete tipo;
+    private ArrayList<Servicio> servicio;
+
+    public Paquete() {
+    }
+
+    public Paquete(tipoPaquete tipo, ArrayList<Servicio> servicio) {
+        this.tipo = tipo;
+        this.servicio = servicio;
+    }
+
+    public ArrayList<Servicio> getServicio() {
+        return servicio;
+    }
+
+    public tipoPaquete getTipo() {
+        return tipo;
+    }
+
+    public void setServicio(ArrayList<Servicio> servicio) {
+        this.servicio = servicio;
+    }
+
+    public void setTipo(tipoPaquete tipo) {
+        this.tipo = tipo;
+    }
+    
+    
+    public void agregarServicio(){
+        
+    }
+    public void removerServicio(){
+        
+    }
     
 }
