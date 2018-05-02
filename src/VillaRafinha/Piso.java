e488bcb0003fc5becf36af0cbaa0d81561715a2a
aacm13 <37373620+aacm13@users.noreply.github.com>
2018-05-01 21:21:57 -0600

    Update Piso.java
    check me out

--- src/VillaRafinha/Piso.java
+++ src/VillaRafinha/Piso.java
@@ -4,14 +4,39 @@
  * and open the template in the editor.
  */
 package VillaRafinha;
-
+import java.util.*;
 
 /**
  *
- * @author daniel
+ * @author aacm13
  */
 public class Piso {
+    private Nivel nivel;
+    private ArrayList<Habitacion> habitacion;
 
+    public Piso() {
+    }
+
+    public Piso(Nivel nivel, ArrayList<Habitacion> habitacion) {
+        this.nivel = nivel;
+        this.habitacion = habitacion;
+    }
+
+    public ArrayList<Habitacion> getHabitacion() {
+        return habitacion;
+    }
+
+    public Nivel getNivel() {
+        return nivel;
+    }
+
+    public void setHabitacion(ArrayList<Habitacion> habitacion) {
+        this.habitacion = habitacion;
+    }
+
+    public void setNivel(Nivel nivel) {
+        this.nivel = nivel;
+    }
     
     
 }
