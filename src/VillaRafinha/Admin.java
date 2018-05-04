8bf43d027f9c611809c23fa50478827146ff87c9
joisgori <37356528+joisgori@users.noreply.github.com>
2018-05-04 08:33:56 -0600

    Update Admin.java
    en proceso de agregar el método para solicitar el log in del o los administradores

--- src/VillaRafinha/Admin.java
+++ src/VillaRafinha/Admin.java
@@ -5,38 +5,65 @@
  */
 package VillaRafinha;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
+ * Usuario, Huesped, Persona, TarjetaCredito, Admin
  *
  * @author Josué González <00034715@uca.edu.sv>
  */
-public class Admin {
-    Usuario usuario;
-    int idAdmin;
-    
-    //constructores
+public class Usuario extends Persona {
 
-    public Admin() {
-    }
+    private final String usuario;
+    private String constraseña;
+    private ArrayList<Persona> usuarios;
 
-    public Admin(Usuario usuario, int idAdmin) {
+//constructores
+    public Usuario(String usuario, String constraseña, String nombre, String iD) {
+        super(nombre, iD);
         this.usuario = usuario;
-        this.idAdmin = idAdmin;
+        this.constraseña = constraseña;
+        usuarios = new ArrayList<Persona>();
     }
-    
-    //métodos getter
 
-    public Usuario getUsuario() {
+//métodos getter
+    public String getUsuario() {
         return usuario;
     }
 
-    public int getIdAdmin() {
-        return idAdmin;
+    public String getConstraseña() {
+        return constraseña;
     }
 
-    //métodos setter
-    public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
+    public ArrayList<Persona> getUsuarios() {
+        return usuarios;
     }
+
+//métodos setter
+
+
+    public void setUsuarios(ArrayList<Persona> usuarios) {
+        this.usuarios = usuarios;
+    }
+
+    public void setConstraseña(String constraseña) {
+        this.constraseña = constraseña;
+    }
+
+    //métodos de uso reales
+    //método para agregar empleados administrador, estos tendrán un su usuario y contaseña; será un máximo de 3 administradores
+    public void add() {
+        Persona usuario = new Persona(constraseña, constraseña);
+        
+        usuarios.add(usuario);
+        
+        Scanner leer = new Scanner(System.in);
+        
+        System.out.println("Ingrese su nombre de usuario y contraseña");
+        
+    }
+}
 
     public void setIdAdmin(int idAdmin) {
         this.idAdmin = idAdmin;
