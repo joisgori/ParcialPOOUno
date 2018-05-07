5c3a836966dc919fe0addb4743f7aae2aea3dc39
joisgori <37356528+joisgori@users.noreply.github.com>
2018-05-07 14:33:12 -0600

    Update Hospedaje.java
    Terminé de validar la excepción para que tomará la cantidad de días puntuales a reservar y que esta no pase de 7

--- src/VillaRafinha/Hospedaje.java
+++ src/VillaRafinha/Hospedaje.java
@@ -14,46 +14,41 @@ import java.util.Objects;
  * @author daniel
  */
 public class Hospedaje {
-    
+
     private LocalDate fechaLlegada;
     private LocalDate fechaSalida;
     private int numDias;
     private Tipo tipo;
     private boolean isSuperior;
     private Huesped huesped;
-    
-    
-    public Hospedaje(LocalDate fLlegada, LocalDate fSalida, Tipo tipo,boolean isSuperior) throws Exception {
+
+    public Hospedaje(LocalDate fLlegada, LocalDate fSalida, Tipo tipo, boolean isSuperior) throws Exception {
 
         Period periodo = Period.between(fLlegada, fSalida);
-        if (periodo.getDays()<7 && !fLlegada.equals(fSalida)){
+        if (periodo.getDays() < 7 && !fLlegada.equals(fSalida)) {
             this.fechaLlegada = fLlegada;
             this.fechaSalida = fSalida;
             this.numDias = periodo.getDays();
             this.tipo = tipo;
-            this.isSuperior=isSuperior;
-        }else {
-            throw new Exception ("Entrada de datos invalida");
+            this.isSuperior = isSuperior;
+        } else {
+            throw new Exception("Entrada de datos invalida");
         }
     }
-    
-  
 
-    
-    
-    public Hospedaje(LocalDate fLlegada, int numDias) throws Exception{
-        if (numDias<=7){
+    public Hospedaje(LocalDate fLlegada, int numDias) throws Exception {
+        if (numDias <= 7) {
             this.fechaLlegada = fLlegada;
             this.fechaSalida = fechaLlegada.plusDays(numDias);
-        }else{
-            throw new Exception("Entrada de datos invalida");
+        } else {
+            //en esta parte se hará una excepción para que se ingrese una cantidad de días válidos, para funcionar con la función recién definida en gestorreservaciones
+            throw new Exception("Entrada de datos inválida");
         }
     }
 
     public Huesped getHuesped() {
         return huesped;
     }
-    
 
     public boolean isIsSuperior() {
         return isSuperior;
@@ -63,7 +58,6 @@ public class Hospedaje {
         this.isSuperior = isSuperior;
     }
 
-    
     public LocalDate getFechaLlegada() {
         return fechaLlegada;
     }
@@ -84,7 +78,6 @@ public class Hospedaje {
         this.tipo = tipo;
     }
 
-
     public void setFechaLlegada(LocalDate fechaLlegada) {
         this.fechaLlegada = fechaLlegada;
     }
@@ -96,7 +89,6 @@ public class Hospedaje {
     public void setNumDias(int numDias) {
         this.numDias = numDias;
     }
-    
 
     @Override
     public int hashCode() {
@@ -126,27 +118,29 @@ public class Hospedaje {
         }
         return true;
     }
-    
- 
-    public boolean llegaAntes(Hospedaje hospedaje){
+
+    public boolean llegaAntes(Hospedaje hospedaje) {
         return hospedaje.fechaLlegada.isBefore(this.fechaLlegada);
     }
-    public boolean llegaDespues(Hospedaje hospedaje){
+
+    public boolean llegaDespues(Hospedaje hospedaje) {
         return hospedaje.fechaLlegada.isAfter(this.fechaSalida);
     }
-    public boolean llegaDurante(Hospedaje hospedaje){
-        return hospedaje.fechaLlegada.isAfter(this.fechaLlegada)&&hospedaje.fechaLlegada.isBefore(this.fechaSalida);
-    }
-   
-    public boolean saleAntes(Hospedaje hospedaje){
-        return hospedaje.fechaSalida.isBefore(this.fechaLlegada);
-    }
-    public boolean saleDespues(Hospedaje hospedaje){
-        return hospedaje.fechaSalida.isAfter(this.fechaSalida);
-    }
-    public boolean saleDurante(Hospedaje hospedaje){
-        return hospedaje.fechaSalida.isAfter(this.fechaLlegada)&&hospedaje.fechaSalida.isBefore(this.fechaSalida);
+
+    public boolean llegaDurante(Hospedaje hospedaje) {
+        return hospedaje.fechaLlegada.isAfter(this.fechaLlegada) && hospedaje.fechaLlegada.isBefore(this.fechaSalida);
     }
 
-    
+    public boolean saleAntes(Hospedaje hospedaje) {
+        return hospedaje.fechaSalida.isBefore(this.fechaLlegada);
+    }
+
+    public boolean saleDespues(Hospedaje hospedaje) {
+        return hospedaje.fechaSalida.isAfter(this.fechaSalida);
+    }
+
+    public boolean saleDurante(Hospedaje hospedaje) {
+        return hospedaje.fechaSalida.isAfter(this.fechaLlegada) && hospedaje.fechaSalida.isBefore(this.fechaSalida);
+    }
+
 }
