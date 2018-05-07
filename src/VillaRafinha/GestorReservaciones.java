4b766fe0d46f71303d48eeedc681840be4033131
joisgori <37356528+joisgori@users.noreply.github.com>
2018-05-07 14:30:14 -0600

    Update GestorReservaciones.java
    Agregué la función que recibe la cantidad de días en entero para hacer la reservación (y),

--- src/VillaRafinha/GestorReservaciones.java
+++ src/VillaRafinha/GestorReservaciones.java
@@ -17,59 +17,52 @@ import java.util.Scanner;
  * @author daniel
  */
 public class GestorReservaciones {
-    
+
     private ArrayList<Reservacion> reservaciones;
     private Scanner scanner = new Scanner(System.in);
-    private static GestorReservaciones gestorReservaciones;  
+    private static GestorReservaciones gestorReservaciones;
     private static int precioBase;
-    
-        
-    
-    private GestorReservaciones(){
-        reservaciones= new ArrayList<>();
+
+    private GestorReservaciones() {
+        reservaciones = new ArrayList<>();
         this.precioBase = 80;
     }
-    
-    
-    public static GestorReservaciones getInstance(){
-        if(gestorReservaciones==null){
-            gestorReservaciones=new GestorReservaciones();
+
+    public static GestorReservaciones getInstance() {
+        if (gestorReservaciones == null) {
+            gestorReservaciones = new GestorReservaciones();
         }
         return gestorReservaciones;
     }
 
-    public  int getPrecioBase() {
+    public int getPrecioBase() {
         return precioBase;
     }
 
     public void setPrecioBase(int precioBase) {
         GestorReservaciones.precioBase = precioBase;
     }
-    
-    
 
     public ArrayList<Reservacion> getReservaciones() {
         return reservaciones;
     }
-    
-    
-    public int getReservacionesDisp(Hospedaje hospedaje, int disponibilidad){
-        
-        if (this.reservaciones.isEmpty()){
-           return disponibilidad;
-        }else
-        {
+
+    public int getReservacionesDisp(Hospedaje hospedaje, int disponibilidad) {
+
+        if (this.reservaciones.isEmpty()) {
+            return disponibilidad;
+        } else {
             for (Reservacion reserv : reservaciones) {
                 if (hospedaje.getTipo() == reserv.getEstancia().getTipo()) {
                     if (reserv.getEstancia().equals(hospedaje)) {
                         disponibilidad--;
-                    }else if (reserv.getEstancia().llegaDurante(hospedaje)||reserv.getEstancia().saleDurante(hospedaje)){
+                    } else if (reserv.getEstancia().llegaDurante(hospedaje) || reserv.getEstancia().saleDurante(hospedaje)) {
                         disponibilidad--;
-                    }else if(reserv.getEstancia().llegaAntes(hospedaje)&&reserv.getEstancia().saleDespues(hospedaje)){
+                    } else if (reserv.getEstancia().llegaAntes(hospedaje) && reserv.getEstancia().saleDespues(hospedaje)) {
                         disponibilidad--;
-                    }else if (hospedaje.llegaDurante(reserv.getEstancia())||hospedaje.saleDurante(reserv.getEstancia())){
+                    } else if (hospedaje.llegaDurante(reserv.getEstancia()) || hospedaje.saleDurante(reserv.getEstancia())) {
                         disponibilidad--;
-                    }else if(hospedaje.llegaAntes(reserv.getEstancia())&&hospedaje.saleDespues(reserv.getEstancia())){
+                    } else if (hospedaje.llegaAntes(reserv.getEstancia()) && hospedaje.saleDespues(reserv.getEstancia())) {
                         disponibilidad--;
                     }
                 }
@@ -78,91 +71,93 @@ public class GestorReservaciones {
                     return disponibilidad;
                 }
             }
-            System.out.println("Hay "+disponibilidad+" habitaciones disponibles");
+            System.out.println("Hay " + disponibilidad + " habitaciones disponibles");
             return disponibilidad;
         }
     }
 
-    
     public void agregarReservacion(Reservacion reservacion) {
-        
+
         reservaciones.add(reservacion);
         System.out.println("Reservacion agregada exitosamente");
         Collections.sort(this.reservaciones);
     }
-    
-    
-    public void verReservaciones(){
-        Iterator<Reservacion> it= reservaciones.iterator();
+
+    public void verReservaciones() {
+        Iterator<Reservacion> it = reservaciones.iterator();
         Reservacion reserv = it.next();
-        
-        LocalDate inicio= LocalDate.now();
-        LocalDate fin= LocalDate.now().plusDays(7);
-        
-        System.out.println("Mostrando desde: "+inicio.toString()+" hasta "+fin.toString()+"\n");
-        
-        while(reserv.getEstancia().getFechaLlegada().isBefore(fin)){
+
+        LocalDate inicio = LocalDate.now();
+        LocalDate fin = LocalDate.now().plusDays(7);
+
+        System.out.println("Mostrando desde: " + inicio.toString() + " hasta " + fin.toString() + "\n");
+
+        while (reserv.getEstancia().getFechaLlegada().isBefore(fin)) {
             System.out.println(reserv.toString());
-            if (it.hasNext()){
-                reserv=it.next();
-            }else{
+            if (it.hasNext()) {
+                reserv = it.next();
+            } else {
                 break;
             }
-        }      
+        }
     }
-    
-    
-    public LocalDate pedirFechar() throws Exception{
-    
+
+    public LocalDate pedirFechar() throws Exception {
+
         String fecha;
-        
+
         System.out.println("Ingrese fecha en formato dd/mm/aaaa: ");
         fecha = scanner.nextLine();
         DateTimeFormatter formateador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        
-        return LocalDate.parse(fecha,formateador);
+
+        return LocalDate.parse(fecha, formateador);
     }
-    
-    
-    public Tipo pedirTipo() throws Exception{
-        
+
+    //AGREGANDO FUNCION PARA PEDIR CANTIDAD DE DÍAS ---------------
+    public int pedirnum() throws Exception {
+        int numdias;
+
+        System.out.println("Ingrese cantidad de días en número entero: ");
+        numdias = scanner.nextInt();
+        return numdias;
+    }
+    //--------------------- yo josué agregué esto
+
+    public Tipo pedirTipo() throws Exception {
+
         int opc;
         Tipo tipo;
         System.out.println("\n1. Habitacion sencilla\n2. Habitacion doble");
         System.out.print("Ingrese tipo de habitacion: ");
-        
+
         opc = scanner.nextInt();
         switch (opc) {
             case 1:
-                tipo=Tipo.SENCILLA;
+                tipo = Tipo.SENCILLA;
                 break;
             case 2:
-                tipo=Tipo.DOBLE;
+                tipo = Tipo.DOBLE;
                 break;
             default:
                 throw new Exception("Ingrese 1 o 2");
         }
-        
-        
 
         return tipo;
 
     }
-    
-    
-    public boolean pedirPiso(){
-    
+
+    public boolean pedirPiso() {
+
         boolean isSuperior;
         int opc;
-        
+
         System.out.println("\n1. Piso normal\n2.Pisos superiores");
         System.out.print("Ingrese una opcion: ");
         opc = scanner.nextInt();
-        isSuperior = !(opc==1);
+        isSuperior = !(opc == 1);
         return isSuperior;
     }
-    
-     
+
     public Reservacion buscarReservacionNomTel(String nombre, String telefono) throws Exception {
         for (Reservacion r : this.reservaciones) {
             if (r.getHuesped().getNombre().equals(nombre) && r.getHuesped().getTelefono().equals(telefono)) {
@@ -173,6 +168,7 @@ public class GestorReservaciones {
         }
         throw new Exception("No se encuentra esa reservacion");
     }
+
     public Reservacion buscarReservacionNombre(String nombre) throws Exception {
         for (Reservacion r : this.reservaciones) {
             if (r.getHuesped().getNombre().equals(nombre)) {
@@ -181,11 +177,5 @@ public class GestorReservaciones {
         }
         throw new Exception("No se encuentra esa reservacion");
     }
-        
-    }
 
-    
-
-
-   
-
+}
